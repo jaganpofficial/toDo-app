ffb21c6d4a3b7e917ee964081059b6ba563b3e1b
Jagan <jaganjnofficial@gmail.com>
2024-02-16 21:47:59 +0530

    added deletion feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
 
 function App() {
-  const [toDos,setToDos] = useState([])
-  const [toDo,setToDo] = useState('')
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState('');
+
   const deleteTodo = (id) => {
-    
     const filteredItem = toDos.filter(todo => todo.id !== id);
-    setToDos([filteredItem])};
+    setToDos(filteredItem);
+  };
+
   return (
     <div className="app">
       <div className="mainHeading">
@@ -19,36 +20,39 @@ function App() {
         <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={()=>setToDos([...toDos,{id:Date.now(), text: toDo,status: false}])} className="fas fa-plus"></i>
+        <input value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={() => setToDos([...toDos, { id: Date.now(), text: toDo, status: false }])} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        
-        {
-          
-          toDos.map((obj)=>{
-          return (<div className="todo">
-          <div className="left">
-            <input onChange={(e)=>{
-              setToDos(toDos.filter(obj2=>{
-                if(obj2.id===obj.id){
-                  obj2.status = e.target.checked
-                }
-                return obj2
-              }))
-            }} value={obj.status} type="checkbox" name="" id="" />
-            <p>{obj.text}</p>
+        {toDos.map((obj) => (
+          <div className="todo" key={obj.id}>
+            <div className="left">
+              <input
+                onChange={(e) => {
+                  setToDos(
+                    toDos.map((obj2) => {
+                      if (obj2.id === obj.id) {
+                        obj2.status = e.target.checked;
+                      }
+                      return obj2;
+                    })
+                  );
+                }}
+                checked={obj.status}
+                type="checkbox"
+                name=""
+                id=""
+              />
+              <p>{obj.text}</p>
+            </div>
+            <div className="right">
+              <i onClick={() => deleteTodo(obj.id)} className="fas fa-times"></i>
+            </div>
           </div>
-          <div className="right">
-            
-            <i onClick={deleteTodo} className="fas fa-times"></i>
-          </div>
-        </div>)
-        }) }
-
-        {toDos.map((obj)=>{
-          if(obj.status){
-            return(<h1>{obj.text}</h1>)
+        ))}
+        {toDos.map((obj) => {
+          if (obj.status) {
+            return <h1 key={obj.id}>{obj.text}</h1>;
           }
           return null;
         })}
